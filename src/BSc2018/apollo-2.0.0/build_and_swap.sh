b5419d200847cf643f94477634377a8dbeac3610
Emil Kurinkitos Jinstrand <emil.jinstrand@gmail.com>
2018-03-15 13:00:00 +0100

    build_and_swap now builds apollo and then our ros module

--- src/BSc2018/apollo-2.0.0/build_and_swap.sh
+++ src/BSc2018/apollo-2.0.0/build_and_swap.sh
@@ -3,15 +3,30 @@
 # This scripts launches the apollo docker with the default image
 # then bulids apollo, launches the modified image and then builds our ROS module
 
-docker/scripts/dev_start.sh
+echo "Starting stock apollo container"
+docker/scripts/dev_start.sh >/dev/null 2>&1
 
 echo "Running apollo.sh build. THIS MAY TAKE A VERY LONG TIME"
 xhost +local:root 1>/dev/null 2>&1
-docker exec -u $USER -it apollo_dev /apollo/apollo.sh build
+docker exec -u $USER -it apollo_dev /apollo/apollo.sh build >/dev/null
 xhost -local:root 1>/dev/null 2>&1
 
-docker stop apollo_dev
+echo "Stopping stock apollo container"
+docker stop apollo_dev >/dev/null 2>&1
 
-docker/scripts/dev_start.sh -image local_dev
+echo "Starting modified container"
+docker/scripts/dev_start.sh -image local_dev >/dev/null 2>&1
 
+xhost +local:root 1>/dev/null 2>&1
+docker exec -u $USER -it apollo_dev /bin/bash -c 'cd /apollo/ros/ \
+    && source /opt/ros/indigo/setup.bash \
+    && catkin_make \
+    && source devel/setup.bash \
+    && rosrun package rosTest'
+xhost -local:root 1>/dev/null 2>&1
+
+echo "Spawning interactive shell"
 docker/scripts/dev_into.sh
+
+echo "Stopping apollo docker"
+docker stop apollo_dev >/dev/null 2>&1
